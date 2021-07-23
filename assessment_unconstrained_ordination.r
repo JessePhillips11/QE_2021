6659bd55e1fcc94c00d53fc3a7d21b428381d02b
JessePhillips11 <jessephillips11.jp@email.com>
2021-07-23 16:42:02 +0200

    Unconstrained Ordination Assessment. 2nd Commit. 23/07/2021.

--- assessment_unconstrained_ordination.r
+++ assessment_unconstrained_ordination.r
@@ -7,18 +7,20 @@
 # INSTRUCTIONS ------------------------------------------------------------
 
   # 1. Using 2 unconstrained ordination techniques, analyse the mite data
-      # SECTION A: PCA -> mite environmental data
+      # SECTION A: nMDS -> mite environmental data
       # SECTION B: CA -> mite species data
 
   # 2. Using 2 other unconstrained ordination techniques, analyse the dune data
-      # SECTION C: PCoA -> dune species data
-      # SECTION D: nMDS -> dune environmental data
+      # SECTION C: PCA -> dune species data (tb-PCA)
+      # SECTION D: PCoA -> dune environmental data
 
 
 # Load Packages -----------------------------------------------------------
 
 library(tidyverse)
 library(vegan)
+library(cluster)
+source("cleanplot.pca.R")
 
 
 # Read in Data ------------------------------------------------------------
@@ -29,84 +31,131 @@ data("dune") # dune species abundance data
 data("dune.env") # dune environmental data
 
 
-# SECTION A: PCA ----------------------------------------------------------
+# SECTION A: nMDS ---------------------------------------------------------
 
 glimpse(mite.env) # Variables are not homogeneous
-mite.env$Substrate <- as.numeric(mite.env$Substrate)
-mite.env$Shrub <- as.numeric(mite.env$Shrub)
-mite.env$Topo <- as.numeric(mite.env$Topo)
 
-mite.env.pca <- rda(mite.env, scale = TRUE)
-summary(mite.env.pca) # Total Inertia = 5, 84% explained by first 3 components
+# Calculate distance matrix with Gower's distance
+mite.env.matrix <- as.matrix(daisy(mite.env, metric = "gower"))
 
-  # PCA Ordination Diagrams
-source("cleanplot.pca.R")
-cleanplot.pca(mite.env.pca, scaling = 1) # Produces a much cleaner plot than biplot() function
-cleanplot.pca(mite.env.pca, scaling = 2)
+# Do nMDS 
+mite.env.nMDS <- metaMDS(mite.env.matrix) 
+mite.env.nMDS # Stress = 0.18 (in between fair and suspect)
 
-  # Findings
-      # Principal component analysis of the environmental variables of the mite 
-      # data produced a total inertia of 5, 67.5% of which is explained by the 
-      # first 2 principal components, and 84.5% explained by the first 3. 
-      # The Scaling 1 biplot shows us that substrate type (Substrate) and 
-      # shrub density (Shrub) contributes significantly to what makes sites 
-      # vary. Sites ordinated closer together tend to have similar environmental
-      # conditions, so sites that are clustered together represent sites that
-      # are similar.
-      # Scaling 2 shows us that substrate type (Substrate) and substrate density
-      # (SubsDens) are more closely related than any other variables. 
+stressplot(mite.env.nMDS)
+
+# Plot sites
+pl1 <- ordiplot(mite.env.nMDS, type = "none", main = "nMDS Mite Environments")
+points(pl1, "sites", pch = 20, col = "black", cex = 1.25)
+text(pl1, "sites", pos = 2, cex = 0.75)
+abline(h = 0, v = 0, lty = 3)
+
+# Do envfit() and plot environmental variables
+mite.env.fit <- envfit(mite.env.nMDS, mite.env)
+plot(mite.env.fit, col = "red", cex = 0.75)
+
+  # FINDINGS
+    # nMDS represents the ordering relationships among objects. 
+    # Tight clusters of sites will be relatively similar in terms of their
+    # environmental variables. 
+    # Sites can be grouped by the environmental variable that predominantly 
+    # impacts it. 
+
 
 # SECTION B: CA -----------------------------------------------------------
+
+head(mite) # Abundance data, appropriate for CA
 
 mite.ca <- cca(mite)
 summary(mite.ca) # Total Inertia = 1.696, 75% explained by first 7 components 
 
   # CA Ordination Diagrams
 CA_plot_1 <- ordiplot(mite.ca, type = "none", scaling = 1, main = "CA Mite Abundances - Scaling 1")
-text(CA_plot_1, "sites", cex = 0.75, col = "navy")
-text(CA_plot_1, "species", cex = 0.75, col = "darkred")
+text(CA_plot_1, "sites", cex = 0.6, col = "navy")
+text(CA_plot_1, "species", cex = 0.5, col = "darkred")
 
 CA_plot_2 <- ordiplot(mite.ca, type = "none", scaling = 2, main = "CA Mite Abundances - Scaling 2")
-text(CA_plot_2, "sites", cex = 0.75, col = "navy")
-text(CA_plot_2, "species", cex = 0.75, col = "darkred")
+text(CA_plot_2, "sites", cex = 0.5, col = "navy")
+text(CA_plot_2, "species", cex = 0.6, col = "darkred")
 
+  # FINDINGS 
+    # Scaling 1 -> ordination of sites:
+    # Tight cluster of sites on the left, all numbered between 5 and 29
+    # Those sites are likely to be comprised of similar species (and relative frequencies of those species)
+    # Brachy and ONOV feature heavily in those sites
+    # The big cluster of species in the top left contributes more highly to those sites than the others
+    # The remaining sites mostly feature in a cluster on the right (31-70)
+    # The species in the bottom right contribute mostly to these sites. 
+    # Scaling 2 -> ordination of species:
+    # Tight cluster of species in the top left will have similar frequencies across the sites
+    # The rest are slightly more dispersed. 
+    # The tight cluster of species is related to the tight cluster of sites from above (5-29)
+    # These species are more likely to be found at these sites.
+    # Conclusion:
+    # There are certain sites (those found in the 5-29 cluster) which have a higher 
+    # mite diversity and abundance than the rest.  
       
 
-# SECTION C: PCoA ---------------------------------------------------------
+# SECTION C: PCA ----------------------------------------------------------
 
-head(dune) # Abundance data -> Bray-Curtis dissimilarity
-dune.pcoa <- capscale(dune ~ 1, distance = "bray")
-summary(dune.pcoa) # Total Inertia = 4.594, 78% explained by first 4 components
+# I'm kinda only doing a PCA for this species data because I'm saving the PCoA
+# for the non-homogeneous environmental data. Also I'm experimenting with the 
+# Hellinger pre-transformation.
 
-  #PCoA Ordination Diagrams
-PCoA_plot_1 <- ordiplot(dune.pcoa, type = "none", scaling = 1, main = "PCoA Dunes - Scaling 1")
-text(PCoA_plot_1, "species", col = "darkred", cex = 0.75)
-text(PCoA_plot_1, "sites", col = "navy", cex = 0.8)
+head(dune) # Abundance data
 
-PCoA_plot_2 <- ordiplot(dune.pcoa, type = "none", scaling = 2, main = "PCoA Dunes - Scaling 2")
-text(PCoA_plot_2, "species", col = "darkred", cex = 0.75)
-text(PCoA_plot_2, "sites", col = "navy", cex = 0.8)
+dune_pa <- decostand(dune, method = "pa") # convert to presence-absence data
+dune_hel <- decostand(dune_pa, method = "hellinger") # transform data to Hellinger distance (immune to double-zero problem)
+
+# Do PCA
+dune.pca <- rda(dune_hel)
+summary(dune.pca)
+
+# Use cleanplot.pca() to create ordination diagrams
+cleanplot.pca(dune.pca, scaling = 1)
+cleanplot.pca(dune.pca, scaling = 2)
+
+  # FINDINGS
+    # Scaling 1 -> distance between objects represents Euclidean distances
+    # Site 1 is close to site 2, 3 close to 4, and so on. There is a general linear
+    # gradient that the sites follow (with some exception)
+    # Species outside circle of equilibrium contribution makes a higher contribution 
+    # to where the sites fall on the plot. 
+    # This amounts to 10 of the 30 species.
+    # Scaling 2 -> angles between species reflect their correlations 
 
 
-
-# SECTION D: nMDS ---------------------------------------------------------
+# SECTION D: PCoA ---------------------------------------------------------
 
 glimpse(dune.env) # Variables are not homogeneous
-dune.env$Moisture <- as.numeric(dune.env$Moisture)
-dune.env$Manure <- as.numeric(dune.env$Manure)
-dune.env$Management <- as.numeric(dune.env$Management)
-dune.env$Use <- as.numeric(dune.env$Use)
 
-  # Data is transformed, but suitable distance matrix still needs to be applied
-dune.env.nmds <- metaMDS(dune.env, distance = "gower") # Gower coefficient used for heterogeneous data sets
-dune.env.nmds # Stress = 0.13
+# calculate distance matrix with Gower's distance
+dune.env.matrix <- as.matrix(daisy(dune.env, metric = "gower"))
 
-  # nMDS Ordination Diagrams
-stressplot(dune.env.nmds, main = "Shepard plot")
+# Do the PCoa
+dune.env.pcoa <- capscale(dune.env.matrix ~ 1) 
+summary(dune.env.pcoa)
 
-ordiplot(dune.env.nmds, type = "text", main = "nMDS Dunes")
-abline(h = 0, v = 0, lty = 3)
+# Plot sites
+pl2 <- ordiplot(dune.env.pcoa, type = "none", main = "PCoA Dune Environments")
+points(pl2, "sites", pch = 20, col = "black", cex = 1.25)
+text(pl2, "sites", pos = 2, cex = 0.75)
 
-plot(dune.env.nmds, type = "text", main = "Goodness of fit")
-points(dune.env.nmds, display = "sites", cex = goodness(dune.env.nmds) * 200) # bigger bubbles indicate a worse fit
-abline(h = 0, v = 0, lty = 3)
+# Do envfit() and plot environmental variables
+dune.env.fit <- envfit(dune.env.pcoa, dune.env)
+plot(dune.env.fit, col = "red", cex = 0.75)
+
+  # FINDINGS 
+    # PCoA provides Euclidean representation of relationships among objects based
+    # on Gower's index 
+    # No obvious linear gradient among the sites.
+    # Environmental variables help explain relationships between sites. 
+    # Moisture and Land-Use levels plot in linear gradients
+    # Thickness of soil (A1) describes the relationship between sites 14 and 15 best,
+    # as they plot away from the other sites. 
+    # "Nature Conservation Management" and "0 Manure" plot together with sites 19 and 20,
+    # which differentiate them from the other sites. 
+    # Biological Farming relates to level 1 Manure and Moisture
+    # Hobby Farming relates to level 2 Manure and Moisture
+    # Standard Farming plots with level 3 Manure and Pasture Use, which explains  
+    # the relationships between the sites in the bottom left of the plot.
